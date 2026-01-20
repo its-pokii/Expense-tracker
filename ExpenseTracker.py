e6f9ed82c3c00a0d58d004cc9f349a0bb8a4a34e
Pokii <mali.benyas@gmail.com>
2026-01-20 20:45:55 +0100

    first commit

---
+++ ExpenseTracker.py
@@ -0,0 +1,14 @@
+print("===============================")
+print("Welcome to the Expense Tracker")
+print("===============================")
+print("1. Show Categories")
+print("2. Add Categories")
+print("3. Remove Categories")
+print("4. Add Entry")
+print("5. Close")
+print("===============================")
+
+choice = int(input("Enter your choice: "))
+
+
+
