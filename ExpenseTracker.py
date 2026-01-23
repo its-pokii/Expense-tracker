9c0b593f4751ec3023fff9698380fd4078fc868a
Pokii <mali.benyas@gmail.com>
2026-01-23 01:37:43 +0100

    the storage systeme

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -3,8 +3,26 @@ import time
 choix =0
 Categories= []
 Entries = []
+Prices = []
 
-def menu():
+f = open("Data.txt",'r+')
+info_lines = f.readlines()
+for line in info_lines:
+    details = line.split("|")
+    print(details)
+    if details[0] in Categories:
+        ind = Categories.index(details[0])
+        Entries[ind].append(details[1])
+        Prices[ind].append(details[2])
+    else:
+        Categories.append(details[0])
+        Entries.append([0])
+        Prices.append([0])
+        ind = Categories.index(details[0])
+        Entries[ind].append(details[1])
+        Prices[ind].append(details[2])
+
+def menu(): #this is the memu bar shown in the beginning
     print("===============================")
     print("Welcome to the Expense Tracker")
     print("===============================")
@@ -18,72 +36,89 @@ def menu():
     print("===============================")
 
 
-def show_categories():
+def show_categories(): #to show the categories
     print("Categories: ", Categories)
 
-def addCategories(NewElement):
+def addCategories(NewElement): #to add a new category
     Categories.append(NewElement)
     Entries.append([0])
+    Prices.append([0])
     print('Category added : ', NewElement)
 
-def removeCategories(Element):
+
+def removeCategories(Element): #to remove the categories I don't want
     print("Categories: ", Categories)
     if Element in Categories:
         Categories.remove(Element)
         print("Category removed: ", Categories)
 
-def AddEntries(indice,element):
+def AddEntries(indice,element,price): #to add new entries
     Entries[indice].append(element)
-    print('Entry added : ', element)
+    Prices[indice].append(price)
+    print('Entry added : ', element, " with price : ", price)
+    category = Categories[indice]
+    f.write(category)
+    f.write("|")
+    f.write(element)
+    f.write("|")
+    f.write(price)
+    f.write("|")
+    f.write("\n")
 
-def RemoveEntries(indice,element):
+def RemoveEntries(indice,element):#to remove the entries
     if element in Entries[indice]:
         Entries[indice].remove(element)
+        Prices[indice].remove(element)
         print('Entry removed : ', element)
 
-while choix!=7:
+while choix!=7: #to close when I click 7
     menu()
     choix = int(input("Enter your choice: "))
-    if choix == 1:
+
+    if choix == 1: #1.show categories
         show_categories()
         time.sleep(1)
 
 
-    elif choix == 2:
+    elif choix == 2: #2.Add Category
         newElement = input("Enter Category you want to add: ")
         addCategories(newElement)
         time.sleep(1)
-    elif choix == 3:
+    elif choix == 3: #3.Remove Category
         Element = input("Enter Category you want to remove: ")
         removeCategories(Element)
         time.sleep(1)
-    elif choix == 4:
+
+    elif choix == 4: #4.Show Entry
         show_categories()
-        indices = input("select your category: ")
-        if indices in Categories:
-            ind = Categories.index(indices)
+        Categ_indices = input("select your category: ")
+        if Categ_indices in Categories:
+            ind = Categories.index(Categ_indices) #to get the index of the category
             print(Entries[ind])
         else:
             print("Invalid choice")
             time.sleep(2)
 
-    elif choix == 5:
+    elif choix == 5: #5.Add Entry
         show_categories()
-        indices = input("select your category: ")
-        if indices in Categories:
-            ind = Categories.index(indices)
+        Categ_indices = input("select your category: ")
+        if Categ_indices in Categories:
+            ind = Categories.index(Categ_indices)
             NewEntry = input("Enter Entry you want to add: ")
-            AddEntries(ind, NewEntry)
+            NewPrice = input("Enter Price: ")
+            AddEntries(ind, NewEntry, NewPrice)
         else:
             print("Invalid choice")
             time.sleep(2)
-    elif choix == 6:
+    elif choix == 6: #6. Remove Entry
         show_categories()
-        indices = input("select your category: ")
-        if indices in Categories:
-            ind = Categories.index(indices)
+        Categ_indices = input("select your category: ")
+        if Categ_indices in Categories:
+            ind = Categories.index(Categ_indices)
             Entry = input("Enter Entry you want to remove: ")
             RemoveEntries(ind, Entry)
         else:
             print("Invalid choice")
             time.sleep(2)
+
+f.close()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ﻿# Expense-tracker
+this is my first of python;
+and in this project i want to creat an expenses tracker to see where my money goes and i will improve it eveytimes until i understand python very well;
+this is just the beginning and inshaa allah i will make better things;
